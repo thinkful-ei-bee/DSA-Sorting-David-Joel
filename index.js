06cb9ca1948429beedd7d4dd79ef90f0deade4ef
David Bolin <david@bolin.at>
2019-05-23 09:38:25 -0700

    working on merge sort with linked list

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+'use strict';
+
+const LinkedList = require('./linkedlist');
+
 function qSort(arr, start = 0, end = arr.length) {
   if (start >= end) {
     return arr;
@@ -21,15 +25,50 @@ function partition(arr, start, end) {
   return j;
 }
 function swap(arr, i, j) {
-  let temp = arr[i]
+  let temp = arr[i];
   arr[i] = arr[j];
   arr[j] = temp;
   return arr;
 }
 
+
+
+
+
+
+
+function mSortList(list, count) {
+  if (!list.head || !list.next) {
+    return list;
+  }
+
+  if (!count) {
+    count = 0;
+    let item = list.head;
+
+    while (item) {
+      count++;
+      item = item.next;
+    }
+  }
+
+  const middle = Math.floor(count / 2);
+  let left = listCut(list, 0, middle);
+  // {
+  //   list: list,
+  //   count: num
+  // }
+  let right = listCut(list, middle, count);
+
+  left = mSortList(left.list, left.count);
+  right = mSortList(right.list, right.count);
+
+  return mergeList(left, right, list);
+}
+
 function mSort(array) {
   if (array.length <= 1) {
-      return array;
+    return array;
   }
 
   const middle = Math.floor(array.length / 2);
@@ -39,32 +78,32 @@ function mSort(array) {
   left = mSort(left);
   right = mSort(right);
   return merge(left, right, array);
-};
+}
 function merge(left, right, array) {
   let leftIndex = 0;
   let rightIndex = 0;
   let outputIndex = 0;
   while (leftIndex < left.length && rightIndex < right.length) {
-      if (left[leftIndex] < right[rightIndex]) {
-          array[outputIndex++] = left[leftIndex++];
-      }
-      else {
-          array[outputIndex++] = right[rightIndex++];
-      }
+    if (left[leftIndex] < right[rightIndex]) {
+      array[outputIndex++] = left[leftIndex++];
+    }
+    else {
+      array[outputIndex++] = right[rightIndex++];
+    }
   }
 
   for (let i = leftIndex; i < left.length; i++) {
-      array[outputIndex++] = left[i];
+    array[outputIndex++] = left[i];
   }
 
   for (let i = rightIndex; i < right.length; i++) {
-      array[outputIndex++] = right[i];
+    array[outputIndex++] = right[i];
   }
   return array;
-};
+}
 
 let input = '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
-let arrNum = input.split(" ").map(i => Number(i));
+let arrNum = input.split(' ').map(i => Number(i));
 
 console.log(mSort(arrNum));
 
